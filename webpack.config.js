32a48befbfdd6a01cb96c8f591905a81e429b545
Hung Vu <hung.vu@acomsolutions.com>
2019-05-07 16:03:33 +0700

    Update webpack to react learning

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require("webpack");
 var path = require("path");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var extractPlugin = new ExtractTextPlugin({
+    filename: 'main.css'
+});
 
 module.exports = {
     mode: 'development',
@@ -8,23 +15,51 @@ module.exports = {
     devtool: debug ? "inline-sourcemap" : false,
     entry: "./src/js/client.js",
     module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /(node_modules|bower_components)/,
-            use:{
-                loader: "babel-loader"
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'img/',
+                        publicPath: 'img/'
+                    }
+                }]
             }
-        }]
+        ]
     },
-    output:{
-        path: __dirname +"/src/",
-        filename: "client.min.js"
+    output: {
+        path: path.resolve(__dirname + '/dist'),
+        filename: 'bundle.js',
+        // publicPath: '/dist'
     },
-    plugins: debug?[]:[
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            mangle:false, sourceMap:false
+    optimization: {
+        minimizer: [new UglifyJsPlugin()],
+    },
+    plugins: [
+        extractPlugin,
+        new webpack.optimize.OccurrenceOrderPlugin(), 
+        new HtmlWebpackPlugin ({
+            template: 'src/index.html'
         }),
+        new CleanWebpackPlugin(['dist'])
     ],
 };
