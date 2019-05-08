82f652383115367c65ddf91ef88a3259c761c629
Hung Vu <hung.vu@acomsolutions.com>
2019-05-08 16:46:35 +0700

    Learning Reactjs

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import '../css/main.scss';
+import Counter from './components/Counter'
+
+ReactDOM.render(<Counter/>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,88 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require("webpack");
 var path = require("path");
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+var CleanWebpackPlugin = require("clean-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ManifestPlugin = require("webpack-manifest-plugin");
 var extractPlugin = new ExtractTextPlugin({
-    filename: 'main.css'
+  filename: "main.css"
 });
 
 module.exports = {
-    mode: 'development',
-    // context: path.join(__dirname, "src"),
-    devtool: debug ? "inline-sourcemap" : false,
-    entry: "./src/js/client.js",
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: extractPlugin.extract({
-                    use: ['css-loader', 'sass-loader']
-                })
-            },
-            {
-                test: /\.html$/,
-                use: ['html-loader']
-            },
-            {
-                test: /\.(jpg|png)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'img/',
-                        publicPath: 'img/'
-                    }
-                }]
+  mode: "development",
+  // context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : false,
+  entry: "./src/js/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: ["css-loader", "sass-loader"]
+        })
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "img/",
+              publicPath: "img/"
             }
+          }
         ]
-    },
-    output: {
-        path: path.resolve(__dirname + '/dist'),
-        filename: 'bundle.js',
-        // publicPath: '/dist'
-    },
-    optimization: {
-        minimizer: [new UglifyJsPlugin()],
-    },
-    plugins: [
-        extractPlugin,
-        new webpack.optimize.OccurrenceOrderPlugin(), 
-        new HtmlWebpackPlugin ({
-            template: 'src/index.html'
-        }),
-        new CleanWebpackPlugin(['dist'])
-    ],
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname + "/dist"),
+    filename: "bundle.js"
+    // publicPath: '/dist'
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  },
+  plugins: [
+    extractPlugin,
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico"
+    }),
+    new CleanWebpackPlugin(["dist"]),
+    new ManifestPlugin({
+      fileName: "manifest.json",
+      writeToFileEmit: true,
+      seed: {
+        short_name: "React App",
+        name: "Create React App Sample",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon"
+          }
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff"
+      }
+    })
+  ]
 };
